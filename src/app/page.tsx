4b3c7c76fc2dedda2469b1495c75e9b84a0c0976
momo1108 <momo1108@daum.net>
2026-01-19 17:19:36 +0900

    modify: 홈페이지 컨테이너의 패딩위치를 인라인 스타일로 변경

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ export default async function Home() {
 
   return (
     <PageLayout>
-      <Container className="pt-6 flex flex-col gap-6 md:gap-10">
+      <Container className="pt-6 px-4 md:px-6 flex flex-col gap-6 md:gap-10">
         {error ? (
           <ErrorAlert message={error} />
         ) : data ? (
